c231fedafef2566c1350924984f2d8e07de1db8a
arbadsha00 <arbadsha00@gmail.com>
2024-11-05 08:53:37 +0600

    statistics done

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+import { useLoaderData } from "react-router-dom";
+import {
+  ComposedChart,
+  Area,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 const Statistics = () => {
-    return (
-        <HelmetProvider>
-            <div>
-            <Helmet>
+  const data = useLoaderData();
+  return (
+    <HelmetProvider>
+      <Helmet>
         <title>Gadget Heaven | Statistics</title>
-    
       </Helmet>
-            Statistics
+      <div className="my-10 container mx-auto px-4">
+        <h1 className="text-2xl font-semibold">Statistics</h1>
+
+        <div className="rounded-xl bg-white mx-auto my-6 py-4">
+          <ResponsiveContainer width="100%" height={400}>
+            <ComposedChart width="100%" height={300} data={data} margin={{}}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="product_title" tick={{ fontSize: 12 }} />
+              <YAxis tick={{ fontSize: 12 }} />
+              <Tooltip />
+              <Legend />
+              <Area
+                baseValue={1}
+                dataKey="price"
+                fill="#efe1fb"
+                stroke="#efe1fb"
+              />
+              <Bar barSize={30} dataKey="price" fill="#8884d8" />
+            </ComposedChart>
+          </ResponsiveContainer>
         </div>
-        </HelmetProvider>
-        
-    );
+      </div>
+    </HelmetProvider>
+  );
 };
 
 export default Statistics;
--- src/main.jsx
+++ src/main.jsx
@@ -37,6 +37,7 @@ const router = createBrowserRouter([
       {
         path: "/statistics",
         element: <Statistics></Statistics>,
+        loader : ()=>fetch("../gadgets.json"),
       },
       {
         path: "/dashboard",
