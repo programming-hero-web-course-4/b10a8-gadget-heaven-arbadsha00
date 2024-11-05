4165d9c94a219ec6fafdb321bfaaebc123d8a66f
arbadsha00 <arbadsha00@gmail.com>
2024-11-05 07:39:17 +0600

    error page updated

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { useRouteError } from 'react-router-dom';
-
+import React from "react";
+import { useRouteError } from "react-router-dom";
+import error404 from "../assets/404.png";
 const Error = () => {
-    const error = useRouteError();
-    return (
-        <div>
-            <h1>Oops!</h1>
+  const error = useRouteError();
+  return (
+    <div className=" bg-white">
+      <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
-        </div>
-    );
+      <img className="mx-auto my-20 object-cover md:h-[400px]" src={error404} alt="" />
+      
+    </div>
+  );
 };
 
 export default Error;
